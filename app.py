37b6744a40a9bee987c40689738b597f66a3ed94
SumitJ2002 <sumitjaiswarsj420@gmail.com>
2024-07-28 14:57:09 +0530

    fourth commit

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ import json
 import sqlite3
 import secrets  
 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
 
@@ -21,7 +26,22 @@ secret_key = secrets.token_hex(24)
 app.secret_key = secret_key  # Change this to a random secret key
 bcrypt = Bcrypt(app)
 
-cred = credentials.Certificate('credentials.json')
+
+firebase_creds = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+}
+
+cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://campnavig-default-rtdb.firebaseio.com/'
 })
@@ -863,7 +883,7 @@ def get_seat(row, col):
         print(e)
         return jsonify({'error': 'Failed to fetch seat'}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True)
 
     
